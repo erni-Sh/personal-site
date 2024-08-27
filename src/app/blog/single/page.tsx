931cda4d404329d5a6d9faf19df493656f1fc7cb
ernest <e.sheremet@macbuildserver.com>
2024-08-27 11:49:21 +0300

    check author

--- src/app/blog/single/page.tsx
+++ src/app/blog/single/page.tsx
@@ -1,5 +1,4 @@
-import React, {FC, JSX} from 'react';
-import SectionComingSoon from '@/components/section-coming-soon';
+import React, { FC, JSX } from 'react';
 import Top from '@/app/blog/single/sections/top';
 import BlogSingle from '@/app/blog/single/sections/blog-single';
 
